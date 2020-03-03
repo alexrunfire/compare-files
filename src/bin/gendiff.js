e08ee26a47672d0f93f10ae216c9e8a449d97382
Alex <severtabwestone@gmail.com>
2020-03-03 23:39:28 +0300

    add -f option, argument list, action func

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,10 @@ const program = require('commander');
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig, secondConfig);
+  });
 program.parse(process.argv);
