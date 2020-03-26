23cfade43dad75fcde00475841b3c0e5a3d0679e
Alex <severtabwestone@gmail.com>
2020-03-26 22:23:17 +0300

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,26 +14,18 @@ const pathToAfterYml = getAbsolutePath('after.yml');
 const pathToBeforeIni = getAbsolutePath('before.ini');
 const pathToAfterIni = getRelativePath('after.ini');
 
+const classicRes = fs.readFileSync(getRelativePath('results/classic'), 'utf-8');
+const plainRes = fs.readFileSync(getRelativePath('results/plain'), 'utf-8');
+const jsonRes = fs.readFileSync(getRelativePath('results/json'), 'utf-8');
+
 describe.each([
   [pathToBeforeJson, pathToAfterJson],
   [pathToBeforeYml, pathToAfterYml],
   [pathToBeforeIni, pathToAfterIni],
 ])('Compare files', (before, after) => {
-  test('classic format', () => {
-    const expectedRes = fs.readFileSync(getRelativePath('results/classic'), 'utf-8');
-    const actualRes = gendiff(before, after, 'classic');
-    expect(actualRes).toBe(expectedRes);
-  });
+  test('classic format', () => expect(gendiff(before, after, 'classic')).toBe(classicRes));
 
-  test('plain format', () => {
-    const expectedRes = fs.readFileSync(getRelativePath('results/plain'), 'utf-8');
-    const actualRes = gendiff(before, after, 'plain');
-    expect(actualRes).toBe(expectedRes);
-  });
+  test('plain format', () => expect(gendiff(before, after, 'plain')).toBe(plainRes));
 
-  test('json format', () => {
-    const expectedRes = fs.readFileSync(getRelativePath('results/json'), 'utf-8');
-    const actualRes = gendiff(before, after, 'json');
-    expect(actualRes).toBe(expectedRes);
-  });
+  test('json format', () => expect(gendiff(before, after, 'json')).toBe(jsonRes));
 });
